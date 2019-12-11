214ff9b9764bb2af6805c851bf05cb2ac8af5b8a
Benwager12 <benwager@live.co.uk>
2019-12-11 14:39:10 +0000

    Implemented time since comments in json. Removed awards.

--- scraper.py
+++ scraper.py
@@ -1,10 +1,12 @@
 import requests, json
 from math import floor
+from time import time
 
 link = "https://www.reddit.com/r/askreddit.json"
 req = requests.get(link, headers={"User-agent":"RedBot 0.1"}).text
 
 data = json.loads(req)["data"]["children"]
+start_time = time()
 
 def getScore(score):
 	if score >= 1000:
@@ -12,6 +14,21 @@ def getScore(score):
 		score = round(score, 1)
 		return str(score)+"k"
 	return str(score)
+
+def getTime(timeScore):
+	divider = ""
+
+	if timeScore < 60:
+		divider = [timeScore, 1, "s"]
+	elif timeScore < 3600:
+		divider = [60, "m"]
+	elif timeScore < 86400:
+		divider = [3600, "h"]
+	elif timeScore < 31536000:
+		divider = [86400, "d"]
+	else: divider = [31536000, "y"]
+
+	return str(round(timeScore/divider[0], 1))+divider[1]
 
 for question in data:
 	question = question["data"]
@@ -21,15 +38,16 @@ for question in data:
 	print("ID: ["+question["id"]+"]")
 	print("Asked by u/"+question["author"]+",")
 	print(question["title"])
+	print(question.keys())
 
 	questionData["title"] = question["title"]
-	questionData["awards"] = question["total_awards_received"]
 
 	print(getScore(question["score"]) + " points\n")
 	questionData["score"] = getScore(question["score"])
 
 	questionlink = question["url"][0:-1]+".json"
 	questionData["url"] = question["url"]
+	questionData["created"] = getTime(start_time-question["created_utc"])
 	
 	try:
 		comments = json.loads(requests.get(questionlink, headers={"User-agent":"RedBot 0.1"}).text)[1]["data"]["children"]
@@ -45,12 +63,15 @@ for question in data:
 			questionData["comments"].append({
 				"author": comment["author"],
 				"score": getScore(comment["score"]),
-				"awards": comment["total_awards_received"],
+				"created": getTime(start_time-comment["created_utc"]),
 				"comment": comment["body"]
 			})
 		except:
 			print("Problem with question, moving on")
 
 	print("Completed, moving on\n")
+
+	#print(json.dumps(questionData))
 	file.write(json.dumps(questionData))
 	file.close()
+	break
